61669a7781dbb64386f7781ea732444a72195dd4
myotintaye <myotintaye00@gmail.com>
2018-03-07 20:38:52 -0500

    #1 added tank movement/animation

---
+++ Assets/TankMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+
+    // Public variables
+    public float maxSpeed = 5f;
+
+    // Private variables
+    private Rigidbody2D rBody;
+    private Animator animator;
+
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        animator.SetFloat("hVelocity", Mathf.Abs(horizontalInput));
+
+        rBody.velocity = new Vector2(horizontalInput * maxSpeed, 0);
+
+        transform.localScale = new Vector3(Mathf.Sign(horizontalInput), 1, 1);
+    }
+}

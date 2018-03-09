d486da55bb7b037f186af1ee998e611fb997b297
Tyler <tylercheng95@gmail.com>
2018-03-09 15:53:06 -0500

    Update Initial. Help, LevelMenu, GameMenu scenes; Create functions for transforming between these scenes

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void Btn_start(string newSelectLevel)
+    {
+        SceneManager.LoadScene(newSelectLevel);
+    }
+
+    public void Btn_help(string newHelp)
+    {
+        SceneManager.LoadScene(newHelp);
+    }
+
+    public void Btn_backHome(string newHome)
+    {
+        SceneManager.LoadScene(newHome);
+    }
+
+    public void Btn_level1(string newTankSelection)
+    {
+        SceneManager.LoadScene(newTankSelection);
+    }
+
+    public void Btn_level2(string newTankSelection)
+    {
+        SceneManager.LoadScene(newTankSelection);
+    }
+
+    public void Btn_level3(string newTankSelection)
+    {
+        SceneManager.LoadScene(newTankSelection);
+    }
+
+    public void Btn_tank1(string newLevelStart)
+    {
+        SceneManager.LoadScene(newLevelStart);
+    }
+
+    public void Btn_tank2(string newLevelStart)
+    {
+        SceneManager.LoadScene(newLevelStart);
+    }
+
+    public void Btn_tank3(string newLevelStart)
+    {
+        SceneManager.LoadScene(newLevelStart);
+    }
+
+    public void Btn_exit()
+    {
+        Application.Quit();
+    }
+}
